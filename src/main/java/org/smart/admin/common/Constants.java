e0939f70cd99badd6a7adc1ca0839055b86b360a
Mohamed Amine Mrad <mradmohamedamine@gmail.com>
2018-05-22 10:16:17 +0200

    french mistake

--- src/main/java/org/smart/admin/common/Constants.java
+++ src/main/java/org/smart/admin/common/Constants.java
@@ -9,7 +9,7 @@ public interface Constants {
 		String QUESTION_CREATE_DOSSIER = "Je veux constituer mon dossier";
 		String QUESTION_COMMUNE = "Quelle est ma préfecture";
 		String QUESTION_REGISTER = "Je veux m'inscrire";
-		String QUESTION_DEPARTMENT = "Quelle est mon département";
+		String QUESTION_DEPARTMENT = "Quel est mon département";
 
 		String WELCOME_MESSAGE = "Bonjour %s ! \n Comment puis-je vous aider ? ";
 
