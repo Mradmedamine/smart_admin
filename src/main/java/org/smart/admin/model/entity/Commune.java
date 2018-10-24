5ffded5b2998122197f513ec2eb0a4c9d4631332
Amine <Amine@DESKTOP-SVO38MP.home>
2018-10-24 23:51:17 +0200

    small fix

--- src/main/java/org/smart/admin/model/entity/Commune.java
+++ src/main/java/org/smart/admin/model/entity/Commune.java
@@ -1,6 +1,5 @@
 package org.smart.admin.model.entity;
 
-import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
@@ -8,27 +7,32 @@ import javax.persistence.Table;
 @Table(name = "communes")
 public class Commune extends BaseEntity {
 
-    private String insee;
-    private String text;
-    private address;
-    
-    public String getInsee() {
-        return insee;
-    }
-    public void setInsee(String insee) {
-        this.insee = insee;
-    }
-    public String getText() {
-        return text;
-    }
-    public void setText(String text) {
-        this.text = text;
-    }
-    public String getAddress() {
-        return address;
-    }
-    public void setAddress(String address) {
-        this.address = address;
-    }
-    
+	private String insee;
+	private String text;
+	private String address;
+
+	public String getInsee() {
+		return insee;
+	}
+
+	public void setInsee(String insee) {
+		this.insee = insee;
+	}
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
 }
