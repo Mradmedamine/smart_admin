1a2c66622cd97f8d064c25aba255bd93859ed551
Mohamed Amine Mrad <mradmohamedamine@gmail.com>
2018-10-24 11:23:44 +0200

    added commune

---
+++ src/main/java/org/smart/admin/model/entity/Commune.java
@@ -0,0 +1,34 @@
+package org.smart.admin.model.entity;
+
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "communes")
+public class Commune extends BaseEntity {
+
+    private String insee;
+    private String text;
+    private address;
+    
+    public String getInsee() {
+        return insee;
+    }
+    public void setInsee(String insee) {
+        this.insee = insee;
+    }
+    public String getText() {
+        return text;
+    }
+    public void setText(String text) {
+        this.text = text;
+    }
+    public String getAddress() {
+        return address;
+    }
+    public void setAddress(String address) {
+        this.address = address;
+    }
+    
+}
